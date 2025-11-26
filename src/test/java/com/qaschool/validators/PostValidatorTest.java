b559a8e5e5ac314aac7e9058a74f019064067c02
sasha17todd <146314986+sasha17todd@users.noreply.github.com>
2025-11-26 14:08:11 +0200

    Refactor postDataProvider test cases

--- src/test/java/com/qaschool/validators/PostValidatorTest.java
+++ src/test/java/com/qaschool/validators/PostValidatorTest.java
@@ -17,26 +17,20 @@ public class PostValidatorTest {
     @DataProvider(name = "postDataProvider")
     public Object[][] postDataProvider() {
 
-        // Construim un text mai lung de 250 caractere
         StringBuilder longPost = new StringBuilder();
         for (int i = 0; i < 260; i++) {
             longPost.append('a');
         }
 
         return new Object[][]{
-                // VALID
                 { "Acesta este un post valid.", "POST_VALID" },
 
-                // EMPTY - null
                 { null, "ERROR_EMPTY" },
 
-                // EMPTY - string gol
                 { "", "ERROR_EMPTY" },
 
-                // FORBIDDEN - conține "politică"
                 { "Acest post vorbește despre politică.", "ERROR_FORBIDDEN" },
 
-                // TOO_LONG - peste 250 caractere
                 { longPost.toString(), "ERROR_TOO_LONG" }
         };
     }
